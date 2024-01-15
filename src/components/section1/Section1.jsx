38b24ea270ac0e949fa2f92bb340ae3d61d89796
dhruv <dhruv18ranjan@gmail.com>
2024-01-15 17:00:55 +0530

    update

--- src/components/section1/Section1.jsx
+++ src/components/section1/Section1.jsx
@@ -7,43 +7,61 @@ const Section1 = ({ targetSectionRef }) => {
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = async () => {
-    if (!email) {
-      console.error("Please enter your email address");
-      return;
-    }
-    const url = "https://gumlet-team.myfreshworks.com/crm/sales/api/contacts";
-    const jsonPayload = {
-      contact: {
-        email: email,
-      },
-    };
-    const headers = {
-      Authorization: "Token token=eQFEevSnl2SGx4uFBp1X8g",
-      "Content-Type": "application/json",
-    };
-    setLoading(true);
+  // const handleSubmit = async () => {
+  //   if (!email) {
+  //     console.error("Please enter your email address");
+  //     return;
+  //   }
+  //   const url = "https://gumlet-team.myfreshworks.com/crm/sales/api/contacts";
+  //   const jsonPayload = {
+  //     contact: {
+  //       email: email,
+  //     },
+  //   };
+  //   const headers = {
+  //     Authorization: "Token token=eQFEevSnl2SGx4uFBp1X8g",
+  //     "Content-Type": "application/json",
+  //   };
+  //   setLoading(true);
 
-    try {
-      const response = await axios.post(url, jsonPayload, { headers });
-      console.log("Success:", response.data);
-    } catch (error) {
-      console.error("Error:", error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+  //   try {
+  //     const response = await axios.post(url, jsonPayload, { headers });
+  //     console.log("Success:", response.data);
+  //   } catch (error) {
+  //     console.error("Error:", error.message);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   if (window.GumletForm) {
+  //     console.log("Gumlet form script loaded successfully!");
+  //   } else {
+  //     console.log("Error loading Gumlet form script.");
+  //   }
+  // }, []);
 
   useEffect(() => {
-    if (window.GumletForm) {
-      console.log("Gumlet form script loaded successfully!");
-    } else {
-      console.log("Error loading Gumlet form script.");
-    }
+    const script = document.createElement("script");
+    script.src =
+      "https://gumlet-team.myfreshworks.com/crm/sales/web_forms/7afc54c6287eef697898c6beef6f39d2555b4dc0831191312f62aa3941e42a3b/form.js";
+    script.crossOrigin = "anonymous";
+    script.id =
+      "fs_7afc54c6287eef697898c6beef6f39d2555b4dc0831191312f62aa3941e42a3b";
+
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
   }, []);
 
   return (
-    <div className="bg-gradient-to-r from-[#291FB1]  to-[rgb(80,70,230)]  ... md:rounded-b-[90%] " ref={targetSectionRef}>
+    <div
+      className="bg-gradient-to-r from-[#291FB1]  to-[rgb(80,70,230)]  ... md:rounded-b-[90%] "
+      ref={targetSectionRef}
+    >
       <div className="md:px-2 lg:px-48 flex flex-col justify-center gap-8 p-6">
         <h1 className=" md:px-12 lg:pr-80 text-3xl md:text-5xl font-semibold text-left  text-white mt-20">
           Best Vimeo Alternative For Video Hosting
@@ -54,11 +72,11 @@ const Section1 = ({ targetSectionRef }) => {
             ensures buffer-free video delivery with each view.
           </span>
           <div className=" flex md:flex-wrap  bg-white rounded-full">
-            <script
+            {/* <script
               src="https://gumlet-team.myfreshworks.com/crm/sales/web_forms/7afc54c6287eef697898c6beef6f39d2555b4dc0831191312f62aa3941e42a3b/form.js"
               crossorigin="anonymous"
               id="fs_7afc54c6287eef697898c6beef6f39d2555b4dc0831191312f62aa3941e42a3b"
-            ></script>
+            ></script> */}
           </div>
         </div>
       </div>
